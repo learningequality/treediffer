4df4f9b864a493e46445e823e256f67c80dd1501
Ivan Savov <ivan.savov@gmail.com>
2020-06-11 09:53:34 -0400

    various old cleanups

--- src/treediffer/cli.py
+++ src/treediffer/cli.py
@@ -1,18 +1,5 @@
 """
 Module that contains the command line app.
-
-Why does this file exist, and why not put this in __main__?
-
-  You might be tempted to import things from __main__ later, but that will cause
-  problems: the code will get executed twice:
-
-  - When you run `python -mtreediffer` python will execute
-    ``__main__.py`` as a script. That means there won't be any
-    ``treediffer.__main__`` in ``sys.modules``.
-  - When you import __main__ it will get executed again (as a module) because
-    there's no ``treediffer.__main__`` in ``sys.modules``.
-
-  Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
 import argparse
 
@@ -23,4 +10,4 @@ parser.add_argument('names', metavar='NAME', nargs=argparse.ZERO_OR_MORE,
 
 def main(args=None):
     args = parser.parse_args(args=args)
-    print(args.names)
+    # print(args.names)
